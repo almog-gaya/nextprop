02f7294d09d54acda1a79ed8d8450b5e18e7f1bf
bahadurh <bahadurhere@gmail.com>
2025-03-06 17:49:12 +0500

    feat(opportunity): fixed all crud operations

--- src/app/api/opportunities/route.ts
+++ src/app/api/opportunities/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { fetchWithErrorHandling } from '@/lib/enhancedApi';
+import { fetchWithErrorHandling, getAuthHeaders } from '@/lib/enhancedApi';
 import { cookies } from 'next/headers';
 
 export async function POST(request: NextRequest) {
@@ -14,16 +14,15 @@ export async function POST(request: NextRequest) {
         assignedTo,
         customFields
     } = await request.json();
-    const data = await fetchWithErrorHandling(await createOpportunity(
-        pipelineId,
-        locationId,
+    const data = await fetchWithErrorHandling(() => createOpportunity(
+        pipelineId, 
         name,
         pipelineStageId,
         status,
         contactId,
         monetaryValue,
         assignedTo,
-        customFields,
+        customFields
     ),
     );
     return NextResponse.json(data);
@@ -31,7 +30,6 @@ export async function POST(request: NextRequest) {
 
 const createOpportunity = async (
     pipelineId: string,
-    locationId: string,
     name: string,
     pipelineStageId: string,
     status: string,
@@ -40,21 +38,35 @@ const createOpportunity = async (
     assignedTo: string,
     customFields: any[]
 ) => {
-    const cookieStore = await cookies();
-    const token = cookieStore.get('ghl_access_token');
-    
+    const { locationId, token } = await getAuthHeaders();
+
     const url = 'https://services.leadconnectorhq.com/opportunities/';
+    const body = {
+        pipelineId,
+        locationId,
+        name,
+        pipelineStageId,
+        status,
+        contactId,
+        monetaryValue,
+        assignedTo,
+        customFields
+    }
     const options = {
         method: 'POST',
         headers: {
-            Authorization: `Bearer ${token?.value}`,
+            Authorization: `Bearer ${token}`,
             Version: '2021-07-28',
             'Content-Type': 'application/json',
             Accept: 'application/json'
         },
-        body: '{"pipelineId":"VDm7RPYC2GLUvdpKmBfC","locationId":"ve9EPM428h8vShlRW1KT","name":"First Opps","pipelineStageId":"7915dedc-8f18-44d5-8bc3-77c04e994a10","status":"open","contactId":"mTkSCb1UBjb5tk4OvB69","monetaryValue":220,"assignedTo":"082goXVW3lIExEQPOnd3","customFields":[{"id":"6dvNaf7VhkQ9snc5vnjJ","key":"my_custom_field","field_value":"9039160788"}]}'
+        body: JSON.stringify(body)
     };
     const response = await fetch(url, options);
     const data = await response.json();
+    /// if response not okay throw error
+    if (!response.ok) {
+        throw new Error(data.message);
+    }
     return data;
 }
