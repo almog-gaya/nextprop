9eaa96df99b8fd6b2ef3b21d8268002967811352
bahadurh <bahadurhere@gmail.com>
2025-03-29 07:28:23 +0500

    feat(bulk-actions): add bulk actions feature with sidebar link, API routes, and UI components
    This commit introduces a new bulk actions feature, including:
    - A sidebar link for bulk actions navigation
    - API routes for fetching bulk actions and logs
    - A bulk actions page for viewing and managing bulk operations
    - A stats popup for detailed bulk action statistics

---
+++ src/app/api/bulk-actions/logs/search/route.ts
@@ -0,0 +1,51 @@
+import { getAuthHeaders } from "@/lib/enhancedApi";
+import { refreshTokenIdBackend } from "@/utils/authUtils";
+
+export async function POST(request: Request) {
+    const url = 'https://services.leadconnectorhq.com/bulk-actions/logs/search';
+    const tokenId = (await refreshTokenIdBackend()).id_token;
+    const {locationId} = await getAuthHeaders();
+    const {requestId, limit, page, filter} = await request.json();
+
+    console.log(`requestId: ${requestId}, limit: ${limit}, page: ${page}, filter: ${filter}`)
+    const payload = {
+        "locationId": locationId,
+        "limit": limit,
+        "page": page,
+        "filters": [
+            {
+                "field": "bulkRequestId",
+                "operator": "eq",
+                "value": requestId
+            },
+            {
+                ...filter
+            }
+        ],
+        "sort": [], 
+    }
+    const headers = {
+        'token-id': tokenId,
+        'sec-ch-ua-platform': '"Android"',
+        'Referer': 'https://app.gohighlevel.com/',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?1',
+        'baggage': 'sentry-environment=production,sentry-release=999e88d7a7b7c61d87dd30f3898b826f1869d5c6,sentry-public_key=c67431ff70d6440fb529c2705792425f,sentry-trace_id=bf1e1e98298a416ea44f43e97044f8ff',
+        'sentry-trace': 'bf1e1e98298a416ea44f43e97044f8ff-aedb6b02a98b58f4',
+        'source': 'WEB_USER',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Mobile Safari/537.36',
+        'Accept': 'application/json, text/plain, */*',
+        'channel': 'APP',
+        'Content-Type': 'application/json',
+        'DNT': '1',
+        'Version': '2021-04-15'
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(payload),
+    });
+    const data = await response.json();
+    return Response.json(data);
+}
