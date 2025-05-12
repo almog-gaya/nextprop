44e8d6b01690dc149f2911345bf2e70480cc730c
almog-gaya <110676355+almog-gaya@users.noreply.github.com>
2025-05-12 16:34:29 +0300

    settings button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -270,7 +270,7 @@ export default function Sidebar({ isMobile, isOpen, onClose }: SidebarProps) {
         href="/settings"
         onClick={(e) => handleNavigation('/settings', e)}
         className={classNames(
-          "w-full mx-4 mb-12 mt-auto h-10 flex items-center justify-start rounded-lg transition-all duration-200 cursor-pointer",
+          "w-full mx-6 mb-12 mt-auto max-w-46 h-10 flex items-center justify-start rounded-lg transition-all duration-200 cursor-pointer",
           pathname === "/settings"
             ? "bg-white text-black"
             : "text-[#1C1C1C] hover:text-[#1C1C1C] hover:bg-white/50"
